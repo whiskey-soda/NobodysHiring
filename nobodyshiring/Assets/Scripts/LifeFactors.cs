5e9b9c01aded9d31be0309df6b158e80cc3b94fb
ben <bmfboom9@gmail.com>
2025-03-14 11:54:54 -0600

    add lifeFactors script with values

---
+++ nobodyshiring/Assets/Scripts/LifeFactors.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class LifeFactors : MonoBehaviour
+{
+    float unpaidBills = 0;
+    public float cleanliness = 0;
+    float housingQuality = 0;
+    float savings = 0;
+    float workstationQuality = 0;
+
+    float weight_unpaidBills = .3f;
+    float weight_cleanliness = .25f;
+    float weight_housingQuality = .2f;
+    float weight_savingsAmount = .15f;
+    float weight_workstationQuality = .1f;
+
+    float motivationDrainMax = 50;
+
+
+
+}
