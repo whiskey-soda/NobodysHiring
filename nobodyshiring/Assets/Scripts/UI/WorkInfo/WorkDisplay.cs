96011c76e1b4fe7175793828e09e2aefffa2de12
ben <bmfboom9@gmail.com>
2025-04-15 14:57:35 -0600

    add work display logic

--- nobodyshiring/Assets/Scripts/UI/WorkInfo/WorkDisplay.cs
+++ nobodyshiring/Assets/Scripts/UI/WorkInfo/WorkDisplay.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class WorkDisplay : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject gigPanel;
+    [SerializeField] GameObject jobPanel;
+    [SerializeField] Transform workInfoDisplayParentTransform;
+
+    WorkManager workManager;
+    private void Start()
     {
-        
+        workManager = WorkManager.Instance;
+
+        workManager.workListUpdated.AddListener(PopulateWorkInfoDisplay);
     }
 
-    // Update is called once per frame
-    void Update()
+    void PopulateWorkInfoDisplay()
     {
-        
+        DeleteAllWorkInfoPanels();
+        CreateWorkInfoPanels();
+    }
+
+    void CreateWorkInfoPanels()
+    {
+        foreach (Job job in workManager.jobs)
+        {
+            GameObject jobInfoPanel = Instantiate(jobPanel, workInfoDisplayParentTransform);
+            jobInfoPanel.GetComponent<JobInfoDisplay>().Configure(job);
+        }
+
+        foreach (Gig gig in workManager.gigs)
+        {
+            GameObject gigInfoPanel = Instantiate(gigPanel, workInfoDisplayParentTransform);
+            gigInfoPanel.GetComponent <GigInfoDisplay>().Configure(gig);
+        }
+    }
+
+    void DeleteAllWorkInfoPanels()
+    {
+        foreach (Transform transform in workInfoDisplayParentTransform.GetComponentsInChildren<Transform>())
+        {
+            if (transform != workInfoDisplayParentTransform)
+            {
+                Destroy(transform.gameObject);
+            }
+        }
     }
 }
