806c45a24ff996abbc1f64e24b3d7cb8206ad022
ben <bmfboom9@gmail.com>
2025-04-22 16:06:26 -0600

    add menu + quit button

---
+++ nobodyshiring/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    private void Awake()
+    {
+        // singleton code
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this);
+        }
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
